55c8d14a8a0ff0daf6de65cd7eb95102d8f5451e
Enzo Pauchet <enzo.pauchet@gmail.com>
2024-11-14 18:52:03 +0100

    feat(homepage) : ajout du hero et responsive

---
+++ pages/home.php
@@ -0,0 +1,8 @@
+<div class="hero-scene">
+    <div class="">
+        <div class="hero-scene-content d-flex gap-5 justify-content-center align-items-center">
+            <img src="/assets/images/logo_arcadia.png"  alt="Logo d'Arcadia">
+            <h1 class="text-primary">Explorez la faune et la flore au cœur d’Arcadia, un zoo éco-responsable</h1>
+        </div>
+    </div>
+</div>
